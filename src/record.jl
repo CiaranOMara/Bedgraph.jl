819c0328b7495ed9906b031301e5e211e296c8cd
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-02-13 21:08:47 +1100

    Reorganise

--- src/record.jl
+++ src/record.jl
@@ -22,26 +22,39 @@ function Record{T}(data) :: Record{T} where T
     return data
 end
 
-function _int(pos)
-    return pos
+function Base.convert(::Type{Record}, str::AbstractString)
+    data = _split_line(str)
+    return Record(data[1], data[2], data[3], data[4])
 end
 
-function _string(str)
-    return str
+function Base.convert(::Type{Record{T}}, str::AbstractString) where T
+    data = _split_line(str)
+    return Record{T}(data[1], data[2], data[3], data[4])
 end
 
-function _real(x)
-    return x
+function Base.convert(::Type{Vector{String}}, record::Record)
+    return String[record.chrom, string(record.first), string(record.last), string(record.value)]
 end
 
-function _int(str::AbstractString)
-    return parse(Float64, str) #Note: conversion to Int is completed when the Record is constructed.
+"Access [`Record`](@ref)'s chrom field."
+function chrom(record::Record)
+    return record.chrom
 end
 
-function _real(str::AbstractString)
-    return parse(Float64, str)
+"Access [`Record`](@ref)'s left position."
+function Base.first(record::Record)
+    return record.first
 end
 
+"Access [`Record`](@ref)'s last position."
+function Base.last(record::Record)
+    return record.last
+end
+
+"Access [`Record`](@ref)'s value."
+function value(record::Record)
+    return record.value
+end
 
 function Base.:(==)(a::Record, b::Record)
     return a.chrom == b.chrom &&
@@ -71,45 +84,27 @@ function Base.isless(a::Record, b::Record, chrom_isless::Function=isless)
     return false
 end
 
-function Base.convert(::Type{Vector{String}}, record::Record)
-    return String[record.chrom, string(record.first), string(record.last), string(record.value)]
-end
-
-function Record(data::AbstractString)
-    return convert(Record, data)
-end
-
-function Base.convert(::Type{Record}, str::AbstractString)
-    data = _split_line(str)
-    return Record(data[1], data[2], data[3], data[4])
-end
-
-function Base.convert(::Type{Record{T}}, str::AbstractString) where T
-    data = _split_line(str)
-    return Record{T}(data[1], data[2], data[3], data[4])
-end
-
-"Access [`Record`](@ref)'s chrom field."
-function chrom(record::Record)
-    return record.chrom
-end
-
-"Access [`Record`](@ref)'s left position."
-function Base.first(record::Record)
-    return record.first
-end
-
-"Access [`Record`](@ref)'s last position."
-function Base.last(record::Record)
-    return record.last
-end
-
-"Access [`Record`](@ref)'s value."
-function value(record::Record)
-    return record.value
-end
-
 ## Internal helper functions.
 function _split_line(line::AbstractString)
     return split(line, r"\s", limit=5, keepempty=false) #Note: may return 5 elements.
+end
+
+function _int(pos)
+    return pos
+end
+
+function _string(str)
+    return str
+end
+
+function _real(x)
+    return x
+end
+
+function _int(str::AbstractString)
+    return parse(Float64, str) #Note: conversion to Int is completed when the Record is constructed.
+end
+
+function _real(str::AbstractString)
+    return parse(Float64, str)
 end
